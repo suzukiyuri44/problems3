9c5c1a31604b533247a11f80d25d78a647384222
rinri <rinri@DESKTOP-QG3A65A>
2025-09-20 00:04:45 +0900

    プロジェクト ファイルを追加します。

---
+++ problems3/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace problems3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        class TestPerson
+        {
+            string name;
+            int age;
+            public TestPerson(string name, int age)
+            {
+                this.name = name;
+                this.age = age;
+                Console.WriteLine("基底クラス");
+                Console.WriteLine("名前：" + name + ", 年齢：" +age);
+
+            }
+        }
+        class TestInfo : TestPerson
+        {
+            int height;
+            int weigth;
+            public TestInfo(string name, int age, int height, int weigth) : base(name, age)
+            {
+                this.height = height;
+                this.weigth = weigth;
+                Console.WriteLine("派生クラス");
+                Console.WriteLine("身長：" + height + " , 体重：" + weigth);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // クラス型 変数名 = new クラス型(引数);
+            TestPerson objTestPerson = new TestPerson("山田",20);
+
+            TestInfo objTestInfo = new TestInfo("山田", 20,170, 60);
+        }
+    }
+}
